8e49477ca4994efa3d0f35a090fca389340c3853
wasim <khwasi.87@gmail.com>
2022-01-18 21:30:23 +0530

    added svg file to img/slider-icon,changes in style.css,style.js,index.html

--- js/style.js
+++ js/style.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
     })
 })
 
-$('.owl-carousel').owlCarousel({
+$('#bigowl').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
@@ -33,3 +33,34 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+var owl = $('#slider');
+owl.owlCarousel({
+    loop:true,
+    nav:false,
+    dots:false,
+    margin:10,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },            
+        960:{
+            items:5
+        },
+        1200:{
+            items:6
+        }
+    }
+});
+owl.on('mousewheel', '.owl-stage', function (e) {
+    if (e.deltaY>0) {
+        owl.trigger('next.owl');
+    } else {
+        owl.trigger('prev.owl');
+    }
+    e.preventDefault();
+});
