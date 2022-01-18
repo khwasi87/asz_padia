05b10563fef920371ca6b99e8e92811bfca218bf
wasim <khwasi.87@gmail.com>
2022-01-18 19:31:37 +0530

    added in img folder,changes in style.css,changes in style.js and index.html

--- js/style.js
+++ js/style.js
@@ -7,4 +7,29 @@ $(document).ready(function() {
     $('.search').click(function() {
         $('.search2').toggleClass('expand')
     })
-}) 
+})
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText: [
+        '<i class="fa fa-angle-left fa-2x" aria-hidden="true"></i>',
+        '<i class="fa fa-angle-right fa-2x" aria-hidden="true"></i>'
+    ],
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
